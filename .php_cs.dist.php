b1a2eaa66ed0c031ac4df8489c3e391de49f731b
Alexandr Chernyaev <bliz48rus@gmail.com>
2021-06-22 04:47:48 +0300

    Update and rename .php_cs.dist to .php_cs.dist.php

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -6,38 +6,46 @@ $finder = Symfony\Component\Finder\Finder::create()
     ->notPath('resources/view/mail/*')
     ->in([
         __DIR__ . '/src',
-        __DIR__ . '/tests',
+        __DIR__ . '/tests', 
     ])
     ->name('*.php')
     ->notName('*.blade.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sortAlgorithm' => 'alpha'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
         'no_unused_imports' => true,
+        'no_whitespace_before_comma_in_array' => true,
         'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline_array' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arrays']],
         'phpdoc_scalar' => true,
         'unary_operator_spaces' => true,
-        'binary_operator_spaces' => true,
-        'blank_line_before_statement' => [
-            'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
+        'binary_operator_spaces' => [
+            'operators' => [
+                '=>' => 'align_single_space',
+            ],
         ],
+        'blank_line_before_statement' => [
+            'statements' => ['continue', 'declare', 'return', 'throw', 'try'],
+        ],
+        'phpdoc_separation' => true,
+        'phpdoc_align' => true,
+        'phpdoc_order' => true,
         'phpdoc_single_line_var_spacing' => true,
         'phpdoc_var_without_name' => true,
         'class_attributes_separation' => [
             'elements' => [
-                'method',
+                'method'  => 'one',
             ],
         ],
         'method_argument_space' => [
-            'on_multiline' => 'ensure_fully_multiline',
-            'keep_multiple_spaces_after_comma' => true,
+            'on_multiline' => 'ignore',
         ],
-        'single_trait_insert_per_statement' => true,
+        'trim_array_spaces' => true,
+        'single_trait_insert_per_statement' => false,
     ])
     ->setFinder($finder);
